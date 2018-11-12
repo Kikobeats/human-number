906ccea7dbbce3d898ac9be91e601c6ff3aea370
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-11-12 21:04:44 +0100

    fix: browser compatible

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const ALPHABET = 'KMGTPEZY'.split('')
 const TRESHOLD = 1e3
 
-module.exports = (n, fn) => {
+module.exports = function (n, fn) {
   n = Math.abs(n)
   let index = 0
   while (n >= TRESHOLD && ++index < ALPHABET.length) { n /= TRESHOLD }
