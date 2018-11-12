717336e7962e9cfc8ace5f70228f08df181f83dd
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-11-12 21:30:48 +0100

    fix: use var instead of let

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const TRESHOLD = 1e3
 
 module.exports = function (n, fn) {
   n = Math.abs(n)
-  let index = 0
+  var index = 0
   while (n >= TRESHOLD && ++index < ALPHABET.length) { n /= TRESHOLD }
   if (fn) n = fn(n)
   return index === 0 ? n : n + ALPHABET[index - 1]
