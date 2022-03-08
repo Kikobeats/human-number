59bbf89407c38cfec9d5665d6b4bbbc8d52d54e9
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-03-08 09:48:41 +0100

    ci: add automate publishing

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const TRESHOLD = 1e3
 
 module.exports = function (n, fn) {
   n = Math.abs(n)
-  var index = 0
-  while (n >= TRESHOLD && ++index < ALPHABET.length) { n /= TRESHOLD }
+  let index = 0
+  while (n >= TRESHOLD && ++index < ALPHABET.length) {
+    n /= TRESHOLD
+  }
   if (fn) n = fn(n)
   return index === 0 ? n : n + ALPHABET[index - 1]
 }
